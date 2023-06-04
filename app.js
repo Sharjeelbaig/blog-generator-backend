a6d4e0d82488da6a94c69cd1a634239ace9c5eed
Sharjeel Baig <02-132212-044@student.bahria.edu.pk>
2023-06-04 16:17:05 -0700

    test

--- app.js
+++ app.js
@@ -20,10 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', generateRouter);
+app.use('/', indexRouter);
 app.use('/generate', generateRouter);
 
 module.exports = app;
-
-
-
